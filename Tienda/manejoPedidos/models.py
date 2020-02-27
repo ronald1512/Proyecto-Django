744cf83468b1d3963331827860043ab0a25a9fa7
Bruno <bcoronado.morales@gmail.com>
2020-02-27 15:09:00 -0600

    Creación de modelos

--- Tienda/manejoPedidos/models.py
+++ Tienda/manejoPedidos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Una clase por cada tabla que se encuentre dentro de la base de datos
+
+class Cliente( models.Model ):
+    nombre = models.CharField( max_length = 30 )
+    direccion = models.CharField( max_length = 50 ) 
+    email = models.EmailField() 
+    telefono = models.IntegerField()
+
+class Producto ( models.Model ):
+    nombre = models.CharField( max_length = 30 )
+    precio = models.IntegerField()
+
+class Pedido( models.Model ):
+    numero = models.IntegerField()
+    fecha = models.DateField()
+
+
+
